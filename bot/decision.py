320c1312e8c89aa393e874398cd76a377c7ceb70
Apostolos Tsano <tolitsano18@gmail.com>
2026-07-06 17:37:50 -0400

    Crafted decision when opponent has a poker playstyle of "Rock"

--- bot/decision.py
+++ bot/decision.py
@@ -21,17 +21,36 @@ def decide(pocket_cards,community_cards, pot, to_call, player_id, num_opponents,
             return
         
     elif stats[5] > 10: # as accurate stats come in go based off stats
-        if stats[3] == "Rock":
+
+        if stats[3] == "Rock": #checks if player is a Rock
+            if(to_call < pot * (0.3)): #checks if it is a small bet
+                threshold = pot_odds * 0.7 #if it is small bet lowers the threshold 
+                if(equity < threshold): # if the W% is still not good then fold
+                    return ('Fold',0, f" Since equity { equity: .0%} vs threshold {threshold: .0%}, then we fold because they dont Bluff")
+                
+                else: # if it is good enough then raise to steal their blind
+                    if(community_cards == []):#check if its in preflop and decide
+                        return ('Raise',0,f"Since equity { equity: .0%} vs threshold {threshold: .0%}, then we steal their call because they over-fold")
+                    else:
+                        return ('Call',0, f" Since equity { equity: .0%} vs threshold {threshold: .0%}, we play safe and call, because our hand is still good.")
+                    
+            elif (to_call > pot * (0.6)): # large == becareful only go with very good hand
+                threshold = pot_odds * 1.4 #if it is a big bet increase the bar required to bet
+                if(equity > threshold): # if the W% is good enough then we call them
+                    return ('Call',0, f"Since equity {equity: .0%} vs threshold {threshold: .0%}, we play safe and call since they probably have a monster too")
+                
+                else: # if its not the we run away
+                    return('Fold',0,f"Since equity {equity: .0%} vs threshold {threshold: .0%}, we play fold because they probably have a better hand than us, and dont bluff")
+                
+        elif stats[3] == "TAG": #checks if player is a Tight Aggresive
             return
-        elif stats[3] == "TAG":
+        elif stats[3] == "LAG": #checks if player is a Loose Aggresive
             return
-        elif stats[3] == "LAG":
+        elif stats[3] == "Calling Station": #checks if player is a Calling Station
             return
-        elif stats[3] == "Calling Station":
+        elif stats[3] == "Maniac": #checks if player is an extreme LAG
             return
-        elif stats[3] == "Maniac":
-            return
-        elif stats[3] == "Fish":
+        elif stats[3] == "Fish": #checks if player is a Fish
             return
         
     return
