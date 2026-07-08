4acd38ad9250adb0b054ba5e886848a61d4fabfe
Apostolos Tsano <tolitsano18@gmail.com>
2026-07-08 02:25:49 -0400

    convert numpy array to int

--- engine/game.py
+++ engine/game.py
@@ -26,4 +26,4 @@ def game(conn, num_opponents, hand_id): # function that takes in connection, num
 
     update_profile(conn, player_id) #update the profile to create archetype
 
-    return payoffs.index(max(payoffs)), pot_size 
+    return int(payoffs.index(max(payoffs))), pot_size 
